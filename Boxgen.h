c6c78676d3319d3ea9770b68adc0d9bfeb66bdc9
yuding@bwpc3 <yuding@bwpc3.foo>
2015-07-24 10:58:31 +0200

    lalala

--- Boxgen.h
+++ Boxgen.h
@@ -23,33 +23,17 @@ class Boxgen
         std::deque<HR> HRodlist; // the list storage the Horizantal Rods;
         std::deque<HR> URodlist; // the list storage the Up Rods;
     	int r,c,h; // size of the volume 'box'
+        int o; // the orientation of the box
     	int length; // length of the rod
        
     public:
-    	MC(long int ST, int LEN, int C, int R, int H, double Z); //(C==X, R==y,H==z)
+    	Boxgen(int Orien,int Len); 
 
     	// ********* Getters********//
 
-    	deque<HR> getVRodlist();
-        deque<HR> getHRodlist();
-        deque<HR> getURodlist();
-    	double getTho() const;
-    	double getQ() const;
-    	double getAaccp() const;
-    	double getDaccp() const;
-        double getNh() const;
-        double getNv() const;
         // ******** Setters ******//
-        void setRodlist(std::deque<HR> RodL);
-
 
     	// ******** Other Functianality *******//
-        void Add(Cells &s,double &prob,double &probav, double &probah, double &probau);
-        void Del(Cells &s,double &prob,double &probdv, double &probdh, double &probdu);
-    	void MCRUN(); 
-        void Zvs_();
-
-    	void plot(const deque<HR>& VRodlist, const deque<HR>& HRodlist,const deque<HR>& URodlist);
 
 };
 
